4ae3f5b342647d23859bc570219557a2cc88a5a8
mlapavic <60083278+mlapavic@users.noreply.github.com>
2020-11-01 10:08:19 -0500

    Added general GET, and basic front end
    A basic GET request for the list of classNames and courseCodes was added. Additionally, a basic setup for the front end was created. Finally, the code was cleaned up with router.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
+const data = require("./Lab3-timetable-data.json");
+
+//Setup serving front-end code
+app.use('/', express.static('static'));
+
+//Setup middleware to do logging
+app.use((req,res,next) => { //for all routes
+    console.log(`${req.method} request for ${req.url}`);
+    next();
 });
+
+router.get('/', (req,res) => {
+    let timetableArray = [];
+    for(i=0; i < data.length;i++){
+        timetableArray.push({
+            "subject": data[i].subject,
+            "className": data[i].className
+        });
+    }
+    res.send(timetableArray);
+});
+
+//Install the router at /api/courses
+app.use('/api/courses', router);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
