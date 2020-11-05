b83ae34f2c5965bc948ec08322abbed02ca1f622
mlapavic <60083278+mlapavic@users.noreply.github.com>
2020-11-04 23:32:15 -0500

    Fully added backend functionality & other improvements
    I decided to go past the deadline in order to continue and increase my knowledge of REST APIs. In this is the full backend functionality

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+//const cors = require('cors');
 const app = express();
+//const bodyParser = require('body-parser');
+
+const { body } = require('express-validator');
+
 const port = 3000;
 const router = express.Router();
 const router1 = express.Router();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+ 
+// Set some defaults
+db.defaults({ schedulesDb: [] }).write()
+
 const data = require("./Lab3-timetable-data.json");
 let timetableArray = [];
-const schedules = [];
+
+const data1 = require("./db.json");
+const e = require('express');
+
+
 
 //Parse data in body as JSON
 router1.use(express.json());
@@ -35,11 +53,28 @@ app.use((req,res,next) => { //for all routes
     next();
 });
 
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Back-end Part 1
 //Get all available subject codes and descriptions
 router.get('/', (req,res) => {
-    res.send(timetableArray);
+    let array1 = [];
+
+    for(i=0; i < timetableArray.length;i++){
+        array1.push({
+            "subject": timetableArray[i].subject,
+            "catalog_nbr": timetableArray[i].catalog_nbr
+        });
+    }
+
+    res.send(array1);
+    //res.send(timetableArray);
 });
 
+//Back-end Part 2
 //Get a course(s) via subject code
 router.get('/:subject_Code', (req,res) => { 
     const subject = req.params.subject_Code;
@@ -47,13 +82,14 @@ router.get('/:subject_Code', (req,res) => {
     const courseArray = [];
     const array1 = [];
     for(i=0;i<timetableArray.length;i++){
-        if(timetableArray[i].subject === subject){
+        if((timetableArray[i].subject).toString() === subject.toString()){
             courseArray.push(timetableArray[i]);
             array1.push(timetableArray[i].catalog_nbr);
         }
     }
-    if(courseArray.length > 0){
+    if(array1.length > 0){
         console.log(`Subject: ${subject} Course(s): ${array1}`);
+        //res.send(array1);
         res.send(courseArray); 
     }
     else{
@@ -61,6 +97,7 @@ router.get('/:subject_Code', (req,res) => {
     }
 });
 
+//Back-end Part 3
 //Get a course via subject code + course code 
 router.get('/:subject_Code/:course_Code', (req,res) => {
     const subject = req.params.subject_Code;
@@ -68,7 +105,7 @@ router.get('/:subject_Code/:course_Code', (req,res) => {
     const courseArray = [];
 
     for(i=0;i<timetableArray.length;i++){
-        if(timetableArray[i].subject === subject && timetableArray[i].catalog_nbr === course){
+        if(timetableArray[i].subject === subject && timetableArray[i].catalog_nbr == course){
             courseArray.push(timetableArray[i]);
         }
     }
@@ -80,6 +117,7 @@ router.get('/:subject_Code/:course_Code', (req,res) => {
     }
 });
 
+//Back-end Part 3
 //Get a course via subject code + course code + component
 router.get('/:subject_Code/:course_Code/:course_Component', (req,res) => {
     const subject = req.params.subject_Code;
@@ -88,7 +126,7 @@ router.get('/:subject_Code/:course_Code/:course_Component', (req,res) => {
     const courseArray = [];
     console.log(component);
     for(i=0;i<timetableArray.length;i++){
-        if(timetableArray[i].subject === subject && timetableArray[i].catalog_nbr === course && timetableArray[i].course_info[0].ssr_component === component){
+        if(timetableArray[i].subject === subject && timetableArray[i].catalog_nbr == course && timetableArray[i].course_info[0].ssr_component === component){
             courseArray.push(timetableArray[i]);
         }
     }
@@ -100,23 +138,160 @@ router.get('/:subject_Code/:course_Code/:course_Component', (req,res) => {
     }
 });
 
- /*router1.put('/:schedules', (req,res) => {
-    const newSchedule = req.body;
-    console.log("Schedule: ", newSchedule);
-    //Add name
-    newSchedule.name = (req.params.name).toString();
-    const schedule = schedules.findIndex(s => s.name === (newSchedule.name).toString());
-    if(schedule < 0){
-        console.log('Creating new schedule');
-        schedules.push(newSchedule);
+//#################################################################################################
+//#################################################################################################
+//#################################################################################################
+//Schedule stuff
+
+
+router1.get('/', (req,res) => {
+    let scheduleVar = db.get('schedulesDb').value();
+    const scheduleCourse = [];
+
+    for(i=0; i < scheduleCourse.length;i++){
+        scheduleCourse.pop();
+    }
+    for(i=0;i<scheduleVar.length;i++){ 
+        scheduleCourse.push({
+            "name": (scheduleVar[i]).sName,
+            "Number_Courses": ((scheduleVar[i]).listOfCourses).length
+        });
+
+
+    }
+    console.log(scheduleCourse);
+    res.send(scheduleCourse);
+});
+
+
+//Get the subject/course pairs for a given schedule
+//Back-end Part 6
+router1.get('/:schedule', (req,res) => {   
+    //This will be the name of the schedule that the user inputs
+    const scheduleName = req.params.schedule;
+
+    if(db.get('schedulesDb').find({sName:scheduleName}).value()){
+        res.send(db.get('schedulesDb').find({sName:scheduleName}));
+    }else{
+        console.log(`${scheduleName} was does not exist!`);
+        return res.status(400).send(`${scheduleName} does not exist!`)
     }
  });
- */
+
+ //Create a schedule with a given schedule name
+//Back-end Part 4
+ router1.post('/:schedule', (req,res) => {   
+     if(!req.params.schedule || req.params.schedule.length > 10){
+        //400 Bad Request
+        return res.status(400).send('Name is required & must be a max of 10 characters!')
+     }
+    
+    //This will be the name of the schedule that the user inputs
+    const scheduleName = req.params.schedule;
+
+    //Clean input
+    for(i=0;i<scheduleName.length;i++){
+        if(scheduleName.charAt(i) == '.' || scheduleName.charAt(i) == '/' || scheduleName.charAt(i) == '<' || scheduleName.charAt(i) == '&'){
+            //400 Bad Request
+            return res.status(400).send('No special characters!')
+        }
+    }
+
+    if(db.get('schedulesDb').find({sName:scheduleName}).value()){
+        return res.status(400).send(`${scheduleName} has already been created!`)
+    }else{
+        db.get('schedulesDb').push({sName:scheduleName, listOfCourses:[]}).write()
+        console.log(`${scheduleName} was created and stored in db!`);
+    }
+
+    //Send this response
+    res.send("Post Test Complete");
+ });
+ 
+
+ //Save a subject/course code pair to a schedule
+ //Back-end Part 5
+ router1.post('/:schedule/:subject_Code/:course_Code', (req,res) => { 
+    //Validate  
+    if(!req.params.schedule || req.params.schedule.length > 10){
+        //400 Bad Request
+        return res.status(400).send('Name is required & must be a max of 10 characters!')
+     }
+     if(!req.params.subject_Code || req.params.subject_Code.length > 8){
+        //400 Bad Request
+        return res.status(400).send('Subject code is required & must be a max of 8 characters!')
+     }
+     if(!req.params.course_Code || req.params.course_Code.length > 5){
+        //400 Bad Request
+        return res.status(400).send('Course code is required & must be a max of 5 characters!')
+     }
+
+    //This will be the name of the schedule that the user inputs
+    const scheduleName = req.params.schedule;
+    const subjectVar = req.params.subject_Code;
+    const courseVar = req.params.course_Code;
+
+    //Clean input
+    for(i=0;i<scheduleName.length;i++){
+        if(scheduleName.charAt(i) == '.' || scheduleName.charAt(i) == '/' || scheduleName.charAt(i) == '<' || scheduleName.charAt(i) == '&'){
+            //400 Bad Request
+            return res.status(400).send('No special characters!')
+        }
+    }
+    for(i=0;i<subjectVar.length;i++){
+        if(subjectVar.charAt(i) == '.' || subjectVar.charAt(i) == '/' || subjectVar.charAt(i) == '<' || subjectVar.charAt(i) == '&'){
+            //400 Bad Request
+            return res.status(400).send('No special characters!')
+        }
+    }
+    for(i=0;i<courseVar.length;i++){
+        if(courseVar.charAt(i) == '.' || courseVar.charAt(i) == '/' || courseVar.charAt(i) == '<' || courseVar.charAt(i) == '&'){
+            //400 Bad Request
+            return res.status(400).send('No special characters!')
+        }
+    }
+
+    let scheduleVar = db.get('schedulesDb').find({sName: scheduleName}).value();
+
+    if(scheduleVar){
+        (scheduleVar.listOfCourses).push({subject:subjectVar, catalog_nbr:courseVar });
+        db.set({schedulesDb:scheduleVar}).write();
+        res.send("Course is added!");
+    }else{
+   
+        return res.status(400).send(`${scheduleName} does not exist!`)
+    }
+
+ });
+
+ //Delete a single schedule with a given name
+ router1.delete('/:schedule', (req,res) => {
+    const scheduleName = req.params.schedule;
+
+    if(db.get('schedulesDb').find({sName:scheduleName}).value()){
+        db.get('schedulesDb').remove({sName:scheduleName}).write()
+        res.send(`${scheduleName} has been deleted!`)
+    }else{
+        return res.status(400).send(`${scheduleName} does not exist!`)
+    }
+ });
+
+ //Delete all schedules
+ router1.delete('/', (req,res) => {
+
+    console.log(db.get('schedulesDb').value().length);
+    for(i=0 ; i < (db.get('schedulesDb').value().length) ;i++){
+        db.get('schedulesDb').pop().write()
+    }
+
+    res.send("Delete Test Complete");
+    //db.get('schedulesDb').pop().write()
+ });
    
 //Install the router at /api/courses
 app.use('/api/courses', router);
 
-app.use('/api/schedule', router1);
+app.use('/api/schedules', router1);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- static/app.js
+++ static/app.js
@@ -1,5 +1,13 @@
 document.getElementById('get-timetable').addEventListener('click', getTimetables);
 
+document.getElementById('create-schedule').addEventListener('click', createSchedule);
+document.getElementById('add-course').addEventListener('click', addCourse);
+document.getElementById('delete-schedule').addEventListener('click', deleteSchedule);
+document.getElementById('delete-all').addEventListener('click', deleteAll);
+document.getElementById('display-schedule').addEventListener('click', displaySchedule);
+
+
+//COURSE STUFF
 function getTimetables(){
     while((document.getElementById("timetables")).firstChild ){
         (document.getElementById("timetables")).removeChild((document.getElementById("timetables")).firstChild );
@@ -45,3 +53,43 @@ function getTimetables(){
     }  
 }
 
+////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+//Schedule Stuff
+
+function createSchedule(){
+
+}
+function addCourse(){
+
+}
+function deleteSchedule(){
+
+}
+function deleteAll(){
+
+}
+
+//Display the schedule
+function displaySchedule(){
+    while((document.getElementById("scheduleView")).firstChild ){
+        (document.getElementById("scheduleView")).removeChild((document.getElementById("scheduleView")).firstChild );
+    }
+
+    const scheduleInput = document.getElementById("scheduleN").value;
+
+    fetch(`/api/schedules/${scheduleInput}`)
+        .then(res => res.json()
+        .then(data => {
+            console.log(data);
+            const l = document.getElementById('scheduleView');
+            data.forEach(e => {
+                const item = document.createElement('li');
+                item.appendChild(document.createTextNode(`${e.subject} ${e.catalog_nbr} ${e.className} 
+                ${e.course_info[0].ssr_component} ${e.course_info[0].start_time} ${e.course_info[0].end_time}
+                ${e.course_info[0].days}`));
+                l.appendChild(item);
+            });
+        }));
+
+}
